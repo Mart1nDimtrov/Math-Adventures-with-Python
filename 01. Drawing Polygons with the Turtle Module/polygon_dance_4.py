783b05d549fa64328f961b07495f8bd4b45a3f18
giggly <giggly@abv.bg>
2020-03-10 23:53:34 +0200

    :

---
+++ 01. Drawing Polygons with the Turtle Module/polygon_dance_4.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+# encapsulate with a function
+def polygon(sides=3,sidelength=100):
+    angle = 360 / sides
+    # use a for loop
+    for i in range(sides):
+        forward(sidelength)
+        right(angle)
+
+# set speed and shape
+shape('turtle')
+speed(0)
+# move the turtle
+for i in range(1,60):
+	polygon(60-i,i//1.5)
+	right(3)
