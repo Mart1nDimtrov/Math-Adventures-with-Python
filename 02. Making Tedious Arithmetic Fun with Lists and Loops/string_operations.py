2abe829e96d2a68ef83f94368df6b1c6f53617d1
giggly <giggly@abv.bg>
2020-03-11 23:44:46 +0200

    Starting with data types and variables

---
+++ 02. Making Tedious Arithmetic Fun with Lists and Loops/string_operations.py
@@ -0,0 +1,12 @@
+message = "hello"
+
+# multiply
+print(message*4)
+
+# add
+print(message+message)
+
+# compare
+print(message==message)
+print(message>message)
+print(message<=message)
---
+++ 02. Making Tedious Arithmetic Fun with Lists and Loops/using_type.py
@@ -0,0 +1,20 @@
+a = True
+print(type(a))
+
+b = 2
+print(type(b))
+
+c = 0.5
+print(type(c))
+
+s_integer = "13"
+print(type(s_integer))
+
+s_float = "0.5"
+print(type(s_integer))
+
+f = float(s_float)
+print(type(f))
+
+
+
